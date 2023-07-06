0c67d66e9f09ce45eda5ce124ec80c252e60589a
RidvanOzturk <ozturkridvan1@gmail.com>
2023-07-06 21:26:39 +0300

    Added some buttons

---
+++ TicketmanProject/AltKirilim/AltKuryeProblemleri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketmanProject.AltKirilim
+{
+    public partial class AltKuryeProblemleri : Form
+    {
+        public AltKuryeProblemleri()
+        {
+            InitializeComponent();
+        }
+    }
+}
