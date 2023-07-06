526602450a59c158c0511550ee35e507cf69353d
yunus5334 <yunus9995353@gmail.com>
2023-07-06 20:54:35 +0300

    added test message

--- TicketmanProject/GirisForm.cs
+++ TicketmanProject/GirisForm.cs
@@ -29,7 +29,7 @@ namespace TicketmanProject
             }
             else { 
             anaForm.Show();
-            this.Hide();
+            this.Hide();//lskdfjlksdf
             }
         }
     }
