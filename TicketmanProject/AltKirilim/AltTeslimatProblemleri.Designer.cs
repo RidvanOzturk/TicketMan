9c02b77a29cb34de2e183e3e33d29e5bf95ff90b
RidvanOzturk <ozturkridvan1@gmail.com>
2023-07-06 20:46:37 +0300

    Fixed some bugs

--- TicketmanProject/AltKirilim/AltTeslimatProblemleri.Designer.cs
+++ TicketmanProject/AltKirilim/AltTeslimatProblemleri.Designer.cs
@@ -28,12 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.AltTeslimatSubeBtn = new System.Windows.Forms.Button();
+            this.AltTeslimatAdresBtn = new System.Windows.Forms.Button();
+            this.AltTeslimatKargoTakipBtn = new System.Windows.Forms.Button();
+            this.AltTeslimatDagıtım = new System.Windows.Forms.Button();
+            this.AltTeslimatAktarmaBtn = new System.Windows.Forms.Button();
+            this.AltTeslimatHasarliBtn = new System.Windows.Forms.Button();
+            this.AltTeslimatEksikBtn = new System.Windows.Forms.Button();
+            this.AltTeslimatKayıpBtn = new System.Windows.Forms.Button();
+            this.AltTeslimatFarklıKisiyeBtn = new System.Windows.Forms.Button();
+            this.AltTeslimatIadeKuryeBtn = new System.Windows.Forms.Button();
+            this.AltTeslimatUcretBtn = new System.Windows.Forms.Button();
+            this.AltTeslimatTeslimatAdresiBtn = new System.Windows.Forms.Button();
+            this.AltTeslimatHizliZamanBtn = new System.Windows.Forms.Button();
+            this.AltTeslimatDigerBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AltTeslimatSubeBtn
+            // 
+            this.AltTeslimatSubeBtn.Location = new System.Drawing.Point(310, 68);
+            this.AltTeslimatSubeBtn.Name = "AltTeslimatSubeBtn";
+            this.AltTeslimatSubeBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatSubeBtn.TabIndex = 0;
+            this.AltTeslimatSubeBtn.Text = "Şube tarafından dağıtıma çıkarılmaması";
+            this.AltTeslimatSubeBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatAdresBtn
+            // 
+            this.AltTeslimatAdresBtn.Location = new System.Drawing.Point(310, 124);
+            this.AltTeslimatAdresBtn.Name = "AltTeslimatAdresBtn";
+            this.AltTeslimatAdresBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatAdresBtn.TabIndex = 1;
+            this.AltTeslimatAdresBtn.Text = "Adrese not bırakılmaması";
+            this.AltTeslimatAdresBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatKargoTakipBtn
+            // 
+            this.AltTeslimatKargoTakipBtn.Location = new System.Drawing.Point(310, 180);
+            this.AltTeslimatKargoTakipBtn.Name = "AltTeslimatKargoTakipBtn";
+            this.AltTeslimatKargoTakipBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatKargoTakipBtn.TabIndex = 2;
+            this.AltTeslimatKargoTakipBtn.Text = "Kargo takip numarasının oluşmaması";
+            this.AltTeslimatKargoTakipBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatDagıtım
+            // 
+            this.AltTeslimatDagıtım.Location = new System.Drawing.Point(310, 236);
+            this.AltTeslimatDagıtım.Name = "AltTeslimatDagıtım";
+            this.AltTeslimatDagıtım.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatDagıtım.TabIndex = 3;
+            this.AltTeslimatDagıtım.Text = "Dağıtıma çıkmadan iade faturasının kesilmesi";
+            this.AltTeslimatDagıtım.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatAktarmaBtn
+            // 
+            this.AltTeslimatAktarmaBtn.Location = new System.Drawing.Point(310, 292);
+            this.AltTeslimatAktarmaBtn.Name = "AltTeslimatAktarmaBtn";
+            this.AltTeslimatAktarmaBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatAktarmaBtn.TabIndex = 4;
+            this.AltTeslimatAktarmaBtn.Text = "Aktarma noktasında kalması";
+            this.AltTeslimatAktarmaBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatHasarliBtn
+            // 
+            this.AltTeslimatHasarliBtn.Location = new System.Drawing.Point(448, 68);
+            this.AltTeslimatHasarliBtn.Name = "AltTeslimatHasarliBtn";
+            this.AltTeslimatHasarliBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatHasarliBtn.TabIndex = 5;
+            this.AltTeslimatHasarliBtn.Text = "Hasarlı teslimat yapılması";
+            this.AltTeslimatHasarliBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatEksikBtn
+            // 
+            this.AltTeslimatEksikBtn.Location = new System.Drawing.Point(448, 124);
+            this.AltTeslimatEksikBtn.Name = "AltTeslimatEksikBtn";
+            this.AltTeslimatEksikBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatEksikBtn.TabIndex = 6;
+            this.AltTeslimatEksikBtn.Text = "Eksik teslimat yapılması";
+            this.AltTeslimatEksikBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatKayıpBtn
+            // 
+            this.AltTeslimatKayıpBtn.Location = new System.Drawing.Point(448, 180);
+            this.AltTeslimatKayıpBtn.Name = "AltTeslimatKayıpBtn";
+            this.AltTeslimatKayıpBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatKayıpBtn.TabIndex = 7;
+            this.AltTeslimatKayıpBtn.Text = "Kayıp statüsünde olması";
+            this.AltTeslimatKayıpBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatFarklıKisiyeBtn
+            // 
+            this.AltTeslimatFarklıKisiyeBtn.Location = new System.Drawing.Point(448, 236);
+            this.AltTeslimatFarklıKisiyeBtn.Name = "AltTeslimatFarklıKisiyeBtn";
+            this.AltTeslimatFarklıKisiyeBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatFarklıKisiyeBtn.TabIndex = 8;
+            this.AltTeslimatFarklıKisiyeBtn.Text = "Farklı kişiye teslim edilmesi";
+            this.AltTeslimatFarklıKisiyeBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatIadeKuryeBtn
+            // 
+            this.AltTeslimatIadeKuryeBtn.Location = new System.Drawing.Point(448, 292);
+            this.AltTeslimatIadeKuryeBtn.Name = "AltTeslimatIadeKuryeBtn";
+            this.AltTeslimatIadeKuryeBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatIadeKuryeBtn.TabIndex = 9;
+            this.AltTeslimatIadeKuryeBtn.Text = "İade için adrese kurye yönlendirilmemesi";
+            this.AltTeslimatIadeKuryeBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatUcretBtn
+            // 
+            this.AltTeslimatUcretBtn.Location = new System.Drawing.Point(586, 68);
+            this.AltTeslimatUcretBtn.Name = "AltTeslimatUcretBtn";
+            this.AltTeslimatUcretBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatUcretBtn.TabIndex = 10;
+            this.AltTeslimatUcretBtn.Text = "Ücret/ödemede problem yaşanması";
+            this.AltTeslimatUcretBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatTeslimatAdresiBtn
+            // 
+            this.AltTeslimatTeslimatAdresiBtn.Location = new System.Drawing.Point(586, 124);
+            this.AltTeslimatTeslimatAdresiBtn.Name = "AltTeslimatTeslimatAdresiBtn";
+            this.AltTeslimatTeslimatAdresiBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatTeslimatAdresiBtn.TabIndex = 11;
+            this.AltTeslimatTeslimatAdresiBtn.Text = "Teslimat adresinin değiştirilmemesi";
+            this.AltTeslimatTeslimatAdresiBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatHizliZamanBtn
+            // 
+            this.AltTeslimatHizliZamanBtn.Location = new System.Drawing.Point(586, 180);
+            this.AltTeslimatHizliZamanBtn.Name = "AltTeslimatHizliZamanBtn";
+            this.AltTeslimatHizliZamanBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatHizliZamanBtn.TabIndex = 12;
+            this.AltTeslimatHizliZamanBtn.Text = "(HızlıTeslimat) Zamanında teslimat yapılmaması";
+            this.AltTeslimatHizliZamanBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatDigerBtn
+            // 
+            this.AltTeslimatDigerBtn.Location = new System.Drawing.Point(586, 236);
+            this.AltTeslimatDigerBtn.Name = "AltTeslimatDigerBtn";
+            this.AltTeslimatDigerBtn.Size = new System.Drawing.Size(132, 50);
+            this.AltTeslimatDigerBtn.TabIndex = 13;
+            this.AltTeslimatDigerBtn.Text = "Diğer";
+            this.AltTeslimatDigerBtn.UseVisualStyleBackColor = true;
+            // 
+            // AltTeslimatProblemleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AltTeslimatDigerBtn);
+            this.Controls.Add(this.AltTeslimatHizliZamanBtn);
+            this.Controls.Add(this.AltTeslimatTeslimatAdresiBtn);
+            this.Controls.Add(this.AltTeslimatUcretBtn);
+            this.Controls.Add(this.AltTeslimatIadeKuryeBtn);
+            this.Controls.Add(this.AltTeslimatFarklıKisiyeBtn);
+            this.Controls.Add(this.AltTeslimatKayıpBtn);
+            this.Controls.Add(this.AltTeslimatEksikBtn);
+            this.Controls.Add(this.AltTeslimatHasarliBtn);
+            this.Controls.Add(this.AltTeslimatAktarmaBtn);
+            this.Controls.Add(this.AltTeslimatDagıtım);
+            this.Controls.Add(this.AltTeslimatKargoTakipBtn);
+            this.Controls.Add(this.AltTeslimatAdresBtn);
+            this.Controls.Add(this.AltTeslimatSubeBtn);
+            this.Name = "AltTeslimatProblemleri";
             this.Text = "AltTeslimatProblemleri";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button AltTeslimatSubeBtn;
+        private System.Windows.Forms.Button AltTeslimatAdresBtn;
+        private System.Windows.Forms.Button AltTeslimatKargoTakipBtn;
+        private System.Windows.Forms.Button AltTeslimatDagıtım;
+        private System.Windows.Forms.Button AltTeslimatAktarmaBtn;
+        private System.Windows.Forms.Button AltTeslimatHasarliBtn;
+        private System.Windows.Forms.Button AltTeslimatEksikBtn;
+        private System.Windows.Forms.Button AltTeslimatKayıpBtn;
+        private System.Windows.Forms.Button AltTeslimatFarklıKisiyeBtn;
+        private System.Windows.Forms.Button AltTeslimatIadeKuryeBtn;
+        private System.Windows.Forms.Button AltTeslimatUcretBtn;
+        private System.Windows.Forms.Button AltTeslimatTeslimatAdresiBtn;
+        private System.Windows.Forms.Button AltTeslimatHizliZamanBtn;
+        private System.Windows.Forms.Button AltTeslimatDigerBtn;
     }
 }
